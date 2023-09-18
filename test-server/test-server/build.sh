1eb39adbd4b26a4e4eec2da6174e926090675fb4
Philipp Leutner <philipp.leutner@assambra.com>
2023-09-18 18:13:38 +0200

    The test server

---
+++ test-server/test-server/build.sh
@@ -0,0 +1,7 @@
+#export EZYFOX_SERVER_HOME=
+mvn -pl . clean install
+mvn -pl test-server-common -Pexport clean install
+mvn -pl test-server-app-api -Pexport clean install
+mvn -pl test-server-app-entry -Pexport clean install
+mvn -pl test-server-plugin -Pexport clean install
+cp test-server-zone-settings.xml $EZYFOX_SERVER_HOME/settings/zones/
---
+++ test-server/test-server/test-server-plugin/src/main/java/com/assambra/plugin/controller/UserLoginController.java
@@ -0,0 +1,52 @@
+package com.assambra.plugin.controller;
+
+import com.assambra.common.entity.User;
+import com.assambra.plugin.service.UserService;
+import com.assambra.plugin.service.WelcomeService;
+
+import com.tvd12.ezyfox.bean.annotation.EzyAutoBind;
+import com.tvd12.ezyfox.bean.annotation.EzySingleton;
+import com.tvd12.ezyfox.core.annotation.EzyEventHandler;
+import com.tvd12.ezyfox.security.EzySHA256;
+import com.tvd12.ezyfoxserver.constant.EzyLoginError;
+import com.tvd12.ezyfoxserver.context.EzyPluginContext;
+import com.tvd12.ezyfoxserver.controller.EzyAbstractPluginEventController;
+import com.tvd12.ezyfoxserver.event.EzyUserLoginEvent;
+import com.tvd12.ezyfoxserver.exception.EzyLoginErrorException;
+import lombok.AllArgsConstructor;
+
+import static com.tvd12.ezyfoxserver.constant.EzyEventNames.USER_LOGIN;
+
+@EzySingleton
+@EzyEventHandler(USER_LOGIN)
+@AllArgsConstructor
+public class UserLoginController extends EzyAbstractPluginEventController<EzyUserLoginEvent> {
+
+    private final UserService userService;
+
+    @Override
+    public void handle(EzyPluginContext ctx, EzyUserLoginEvent event) {
+        logger.info("{} login in", event.getUsername());
+
+        String username = event.getUsername();
+        String password = encodePassword(event.getPassword());
+
+        User user = userService.getUser(username);
+
+        if (user == null) {
+            logger.info("User doesn't exist in db, create a new one!");
+            user = userService.createUser(username, password);
+            userService.saveUser(user);
+        }
+
+        if (!user.getPassword().equals(password)) {
+            throw new EzyLoginErrorException(EzyLoginError.INVALID_PASSWORD);
+        }
+
+        logger.info("user and password match, accept user: {}", username);
+    }
+
+    private String encodePassword(String password) {
+        return EzySHA256.cryptUtfToLowercase(password);
+    }
+}
